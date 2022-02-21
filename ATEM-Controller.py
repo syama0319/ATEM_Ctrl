779961fc428b0dae204e8a88e1d38cb9dab460cc
syama0319 <fjhss0036@gmail.com>
2022-02-21 21:41:28 +0900

    read setting-data from CSV!

--- ATEM-Controller.py
+++ ATEM-Controller.py
@@ -2,172 +2,40 @@ import time
 import argparse
 import mido
 import PyATEMMax
-import asyncio
+import csv
 from typing import Dict, Any
 from mido import Message
 from tabulate import WIDE_CHARS_MODE, tabulate
 
-#引数の追加
-#実行時に $ python3 なんたら.py -ip [ip address]とすることでscan()を実行しなくて済む
-#ex.) $ python3 scan.py -ip 192.168.1
-#-hオプションをつけるとhelpメッセージがでる
-parser = argparse.ArgumentParser()
-parser.add_argument('-i','--ip', help='IP address (ex: 192.168.10.240')
-parser.add_argument('-m', '--model', help='MIDI pad model (ex: lpx)')
-args = parser.parse_args()
-
 #Switcherオブジェクトの生成
 switcher = PyATEMMax.ATEMMax()
-mes = 0
 
-#LaunchpadとATEMのボタン対応
-pvwin = ('33','34','35','36','37','29','2A','2B','2C','2D')
-pgmin = ('51','52','53','54','55','47','48','49','4A','4B')
-inputs = ('input1', 'input2', 'input3', 'input4', 'input5', 'input6', 'input7', 'input8', 'input9', 'input10')
-cols = ('black', 'colorBars', 'color1', 'color2', 'mediaPlayer1', 'mediaPlayer2')
-pvwcol = ('3B','31','2F','30','39','3A') #colは Black ColorBars Color mediaPlayer1 mediaPlayer2の順
-pgmcol = ('59','4F','4D','4E','57','58')
-ftb = '45'
-cutme = '10'
-autome = '12'
-fxsty = ('19','1A','1B','1C','1D')
-fxstys = ('mix','dip','wipe','sting','dVE')
-pvwtrans = '13'
-mpties = ('15','0B') #mp1, mp2, dskで使うのはほとんどmpなのでdskとの区別のためmpと表記
-mpairs = ('16', '0C')
-mpautos = ('17', '0D')
-keyers = ('keyer1','keyer2','keyer3','keyer4')
-uskairs = [False, False, False, False]
-uskflys = [False, False, False, False]
-dsks = ("dsk1", "dsk2")
-mptie = [False, False]
-mpair = [False, False]
-mpauto = [False, False]
+#CSVから設定やらのリスト読み込み
+setting_file = open(f"./settings.csv", encoding="utf_8")
+setting_list = csv.reader(setting_file)
+setting_dict = {}
+for row in setting_list:
+    setting_dict[row[0]] = row[1]
+f = csv.reader(open(f"./{setting_dict['button file']}", encoding="utf_8"))
 
-#MIDIのポート設定
-# ports = mido.open_output('Launchpad X LPX MIDI In')
-if args.model == 'lpx':
-    outport = mido.open_output('Launchpad X LPX MIDI In')
-    inport = mido.open_input('Launchpad X LPX MIDI Out')
-else:
-    print(f"[{time.ctime()}] Scanning MIDI devices.")
-    ports_o = mido.get_output_names()
-    print(f"[{time.ctime()}] FINISHED: {len(ports_o)} MIDI Input devices found.")
-    print(f"[{time.ctime()}] {ports_o}")
-    ports_i = mido.get_input_names()
-    print(f"[{time.ctime()}] FINISHED: {len(ports_i)} MIDI Input devices found.")
-    print(f"[{time.ctime()}] {ports_i}")
-    port_on = int(input('Which MIDI Output device? [int] : '))
-    port_in = int(input('Which MIDI Input device? [int] : '))
-    outport = mido.open_output(ports_o[port_on])
-    inport = mido.open_input(ports_i[port_in])
-
-#MIDIPadのデフォの色
-def defclr():
-    for i in pvwin:
-        outport.send(Message.from_hex('90 '+ i +' 15'))
-    for i in pgmin:
-        outport.send(Message.from_hex('90 '+ i +' 05'))
-    for i in range(len(pgmcol)):
-        outport.send(Message.from_hex('90 '+pvwcol[i]+' 18'))
-        outport.send(Message.from_hex('90 '+pgmcol[i]+' 04'))
-    for i in fxsty:
-        outport.send(Message.from_hex('90 '+ i +' 0C'))
-    for i in mpties:
-        outport.send(Message.from_hex('90 '+ i +' 0C'))
-    for i in mpairs:
-        outport.send(Message.from_hex('90 '+ i +' 05'))
-    for i in mpautos:
-        outport.send(Message.from_hex('90 '+ i +' 05'))
-    outport.send(Message.from_hex('90 '+ftb+' 03'))
-    outport.send(Message.from_hex('90 '+cutme+' 09'))
-    outport.send(Message.from_hex('90 '+autome+' 09'))
-    outport.send(Message.from_hex('90 '+pvwtrans+' 22'))
-
-#ATEMの状態を取得
-pgmcam_old = 'old'
-pvwcam_old = 'old'
-def syncStatus():
-    pgmcam = str(switcher.programInput[0].videoSource)
-    pvwcam = str(switcher.previewInput[0].videoSource)
-    style = str(switcher.transition[0].style)
-    
-    for i in range(0,1):
-        mptie[i] = switcher.downstreamKeyer[dsks[i]].tie
-        mpair[i] = switcher.downstreamKeyer[dsks[i]].onAir
-        mpauto[i] = switcher.downstreamKeyer[dsks[i]].isAutoTransitioning
-    """print(type(switcher.downstreamKeyer[dsks[0]].tie))
-    print(switcher.downstreamKeyer[dsks[0]].tie)
-    print(type(switcher.downstreamKeyer[dsks[0]].onAir))
-    print(switcher.downstreamKeyer[dsks[0]].tie)
-    print(type(switcher.downstreamKeyer[dsks[0]].isAutoTransitioning))
-    print(switcher.downstreamKeyer[dsks[0]].tie)"""
-
-    #ボタンを光らせる
-    #PGMボタン
-    if pgmcam in inputs:
-        num = inputs.index(pgmcam)
-        outport.send(Message.from_hex(f'90 {pgmin[num]} 03'))
-
-    if pgmcam in cols:
-        num = cols.index(pgmcam)
-        outport.send(Message.from_hex(f'90 {pgmcol[num]} 03'))
-    
-    if pvwcam in inputs:
-        num = inputs.index(pvwcam)
-        outport.send(Message.from_hex(f'90 {pvwin[num]} 03'))
-
-    if pvwcam in cols:
-        num = cols.index(pvwcam)
-        outport.send(Message.from_hex(f'90 {pvwcol[num]} 03'))
-    
-    if style in fxstys:
-        num = fxstys.index(style)
-        outport.send(Message.from_hex(f'90 {fxsty[num]} 03'))
-    
-    for i in range(0,1):
-        if mptie[i] == True:
-            outport.send(Message.from_hex(f'90 {mpties[i]} 03'))
-        if mpair[i] == True:
-            outport.send(Message.from_hex(f'90 {mpairs[i]} 03'))
-        if mpauto[i] == True:
-            outport.send(Message.from_hex(f'91 {mpautos[i]} 03'))    
-
-    print(f"[{time.ctime()}] ATEM-MIDIpad synced")
-
-###########   Eventのログを表示   ###########
-#たぶん，非同期処理がなくともOK?
-def onConnectAttempt(params: Dict[Any, Any]) -> None:
-    """Called when a connection is attempted"""
-    outport.send(Message.from_hex('92 63 0D'))
-    print(f"[{time.ctime()}] Trying to connect to switcher at {params['switcher'].ip}")
-
-def onConnect(params: Dict[Any, Any]) -> None:
-    """Called when the switcher is connected"""
-    outport.send(Message.from_hex('90 63 03'))
-    print(f"[{time.ctime()}] Connected to switcher {switcher.atemModel} at {params['switcher'].ip}")
-
-def onDisconnect(params: Dict[Any, Any]) -> None:
-    """Called when the switcher disconnects"""
-    outport.send(Message.from_hex('91 63 05'))
-    print(f"[{time.ctime()}] DISCONNECTED from switcher at {params['switcher'].ip}")
-
-def onReceive(params: Dict[Any, Any]) -> None:
-    """Called when data is received from the switcher"""
-    defclr()
-    syncStatus()
-    print(f"[{time.ctime()}] Received [{params['cmd']}]: {params['cmdName']}")
-
-def onWarning(params: Dict[Any, Any]) -> None:
-    """Called when a warning message is received from the switcher"""
-
-    print(f"[{time.ctime()}] Received warning message: {params['cmd']}")
-
-switcher.registerEvent(switcher.atem.events.connectAttempt, onConnectAttempt)
-switcher.registerEvent(switcher.atem.events.connect, onConnect)
-switcher.registerEvent(switcher.atem.events.disconnect, onDisconnect)
-switcher.registerEvent(switcher.atem.events.receive, onReceive)
-switcher.registerEvent(switcher.atem.events.warning, onWarning)
+#ボタンやらの変数，定数リスト
+me = 0
+header = next(f)
+buttonlist = [str(row[3]) for row in f]
+pgmin = tuple(buttonlist[0:10])
+pgmcol = tuple(buttonlist[10:16])
+pvwin = tuple(buttonlist[16:26])
+pvwcol = tuple(buttonlist[26:32])
+uskonair = tuple(buttonlist[32:36])
+usknext = tuple(buttonlist[36:40])
+dsktie = tuple(buttonlist[40:42])
+dskonair = tuple(buttonlist[42:44])
+dskauto = tuple(buttonlist[44:46])
+transitionstyle = tuple(buttonlist[46:51])
+cutme = buttonlist[51]
+autome = buttonlist[52]
+pvwtrans = buttonlist[53]
+ftb = buttonlist[54]
 
 ###########   ATEMへ接続   ###########
 ips = []
@@ -200,19 +68,59 @@ def atem_connect(inip):
                 atem_scan()
             continue
 
-#実行時にipを入れればatem_scan()はスキップされる
-if args.ip is not None :
-    switcher.connect(args.ip)
-else:
+#settingsにipを入れればatem_scan()はスキップされる
+try:
+    print(f"[{time.ctime()}] Connecting ATEM...")
+    switcher.connect(setting_dict['ip address'])
+except:
     atem_scan()
     atem_connect(input('Input IP address'))
 
-#MIDIパッド初期化
+###########   MIDI padの設定   ###########
+#MIDIのポート設定
+# ports = mido.open_output('Launchpad X LPX MIDI In')
+try:
+    outport = mido.open_output(setting_dict['MIDI input'])
+    inport = mido.open_input(setting_dict['MIDI output'])
+except:
+    print(f"[{time.ctime()}] Scanning MIDI devices.")
+    ports_o = mido.get_output_names()
+    print(f"[{time.ctime()}] FINISHED: {len(ports_o)} MIDI Input devices found.")
+    print(f"[{time.ctime()}] {ports_o}")
+    ports_i = mido.get_input_names()
+    print(f"[{time.ctime()}] FINISHED: {len(ports_i)} MIDI Input devices found.")
+    print(f"[{time.ctime()}] {ports_i}")
+    port_on = int(input('Which MIDI Output device? [int] : '))
+    port_in = int(input('Which MIDI Input device? [int] : '))
+    outport = mido.open_output(ports_o[port_on])
+    inport = mido.open_input(ports_i[port_in])
+
+#MIDIPadのデフォの色
+def defclr():
+    for i in pvwin:
+        outport.send(Message.from_hex('90 '+ i +' 15'))
+    for i in pgmin:
+        outport.send(Message.from_hex('90 '+ i +' 05'))
+    for i in range(len(pgmcol)):
+        outport.send(Message.from_hex('90 '+pvwcol[i]+' 18'))
+        outport.send(Message.from_hex('90 '+pgmcol[i]+' 04'))
+    for i in transitionstyle:
+        outport.send(Message.from_hex('90 '+ i +' 0C'))
+    for i in dsktie:
+        outport.send(Message.from_hex('90 '+ i +' 0C'))
+    for i in dskonair:
+        outport.send(Message.from_hex('90 '+ i +' 05'))
+    for i in dskauto:
+        outport.send(Message.from_hex('90 '+ i +' 05'))
+    outport.send(Message.from_hex('90 '+ftb+' 03'))
+    outport.send(Message.from_hex('90 '+cutme+' 09'))
+    outport.send(Message.from_hex('90 '+autome+' 09'))
+    outport.send(Message.from_hex('90 '+pvwtrans+' 22'))
+
 def initialize():
     print(f"[{time.ctime()}] Initializing MIDI pad...")
     #Launchpad XのProgrammer Mode化
     outport.send(Message.from_hex('F0 00 20 29 02 0C 0E 01 F7'))
-    outport.send(Message('clock', 48))
     #Launchpad XのVelocity無効化
     outport.send(Message.from_hex('F0 00 20 29 02 0C 04 03 7F F7'))
     #Launchpad XのAftertouch無効化
@@ -223,66 +131,3 @@ def initialize():
     for i in range(1, 99):
         outport.send(Message('note_on', channel = 0, note = i, velocity = 0))
     defclr()
-
-#終了時にやること
-def end_seq():
-    print(f"[{time.ctime()}] Shutdown...")
-    #ATEMとの接続解除
-    switcher.disconnect()
-    #Launchpad XのAftertouch有効化
-    outport.send(Message.from_hex('F0 00 20 29 02 0C 0B 00 01 F7'))
-    #Launchpad XのLive Mode化
-    outport.send(Message.from_hex('F0 00 20 29 02 0C 0E 00 F7'))
-    outport.close()
-    print(f"[{time.ctime()}] BYE!")
-    quit()
-
-initialize()
-
-#MIDIパッドからの入力をATEMに反映
-while True:
-    msgh = inport.receive().hex().split()
-    #ボタンを離したときは無視する
-    if msgh[2] == '00':
-        continue
-    if msgh[1] in pgmin:
-            num = pgmin.index(msgh[1]) +1
-            switcher.setProgramInputVideoSource(0, num)
-            print(f"[{time.ctime()}]  PGM Video source: {num}")
-    elif msgh[1] in pvwin:
-        num = pvwin.index(msgh[1]) +1
-        switcher.setPreviewInputVideoSource(0, num)
-        print(f"[{time.ctime()}]  PVW Video source: {num}")
-    elif msgh[1] in pgmcol:
-        num = pgmcol.index(msgh[1])
-        switcher.setProgramInputVideoSource(0, cols[num])
-        print(f"[{time.ctime()}]  PGM Video source: " + cols[num])
-    elif msgh[1] in pvwcol:
-        num = pvwcol.index(msgh[1])
-        switcher.setPreviewInputVideoSource(0, cols[num])
-        print(f"[{time.ctime()}]  PVW Video source: " + cols[num])
-    elif msgh[1] in fxsty:
-        num = fxsty.index(msgh[1])
-        switcher.setTransitionStyle(0, fxstys[num])
-    elif msgh[1] == ftb:
-        if switcher.fadeToBlack[0].state.fullyBlack == False and switcher.fadeToBlack[0].state.inTransition:
-            switcher.execFadeToBlackME(0)
-            outport.send(Message.from_hex('91 '+ftb+' 05'))
-    elif msgh[1] == autome:
-        switcher.execAutoME(0)
-    elif msgh[1] == cutme:
-        switcher.execCutME(0)
-    elif msgh[1] in mpties:
-        num = mpties.index(msgh[1])
-        mptie[num] != mptie[num]
-        switcher.setDownstreamKeyerTie(dsks[num], mptie[num])
-    elif msgh[1] in mpairs:
-        num = mpairs.index(msgh[1])
-        mpair[num] != mpair[num]
-        switcher.setDownstreamKeyerTie(dsks[num], mpair[num])
-    elif msgh[1] in mpautos:
-        num = mpautos.index(msgh[1])
-        mpautos[num] != mpauto[num]
-        switcher.setDownstreamKeyerTie(dsks[num], mpauto[num])
-        
-
--- Python_test/csvtest.py
+++ Python_test/csvtest.py
@@ -1,7 +1,90 @@
 import csv
-csv_file = open("./list.csv", encoding="utf_8")
-f = csv.reader(csv_file)
-header = next(f)
-buttonlist = [str(row[2]) for row in f]
+import time
+import mido
+from mido import Message
+from sqlalchemy import true
 
-print(buttonlist)
+with open(f"./settings.csv", encoding="utf_8") as setting_file:
+    setting_list = csv.reader(setting_file)
+    setting_dict = {}
+    for row in setting_list:
+        setting_dict[row[0]] = row[1]
+
+function_list = {}
+button_call = {}
+button_list = []
+def set_list(num) -> None:
+    with open(f"./{setting_dict['profile'+str(num)]}", encoding="utf_8") as list_file:
+        f = csv.reader(list_file)
+        header = next(f) #ヘッダー行を除く
+        for row in f:
+            #key: button, contents: function, value, color
+            function_list[str(row[4])] = [str(row[1]), str(row[2]), str(row[3])]
+            button_call[(str(row[1]), str(row[2]))] = [str(row[4]), str(row[3])]
+            button_list.append(str(row[4]))
+
+###########   MIDI padの設定   ###########
+#MIDIのポート設定
+try:
+    print(f"[{time.ctime()}] Connecting MIDI pad...")
+    outport = mido.open_output(setting_dict['MIDI input'])
+    inport = mido.open_input(setting_dict['MIDI output'])
+    print(f"[{time.ctime()}] MIDI pad connected!")
+except:
+    print(f"[{time.ctime()}] Scanning MIDI devices.")
+    ports_o = mido.get_output_names()
+    print(f"[{time.ctime()}] FINISHED: {len(ports_o)} MIDI Input devices found.")
+    print(f"[{time.ctime()}] {ports_o}")
+    ports_i = mido.get_input_names()
+    print(f"[{time.ctime()}] FINISHED: {len(ports_i)} MIDI Input devices found.")
+    print(f"[{time.ctime()}] {ports_i}")
+    port_on = int(input('Which MIDI Output device? [int] : '))
+    port_in = int(input('Which MIDI Input device? [int] : '))
+    outport = mido.open_output(ports_o[port_on])
+    inport = mido.open_input(ports_i[port_in])
+    print(f"[{time.ctime()}] MIDI pad connected!")
+
+#MIDIPadのデフォの色
+def default_color() -> None:
+    temp_list = list(filter(None, button_list))
+    for i in temp_list:
+        outport.send(Message.from_hex(f'90 {i} {function_list[i][2]}'))
+
+def initialize() -> None:
+    print(f"[{time.ctime()}] Initializing MIDI pad...")
+    #Launchpad XのProgrammer Mode化
+    outport.send(Message.from_hex('F0 00 20 29 02 0C 0E 01 F7'))
+    #Launchpad XのVelocity無効化
+    outport.send(Message.from_hex('F0 00 20 29 02 0C 04 03 7F F7'))
+    #Launchpad XのAftertouch無効化
+    outport.send(Message.from_hex('F0 00 20 29 02 0C 0B 02 01 F7'))
+    #Launchpad XのLED有効化
+    outport.send(Message.from_hex('F0 00 20 29 02 0C 0A 01 01 F7'))
+    #とりま全キーを真っ暗に
+    for i in range(1, 99):
+        outport.send(Message('note_on', channel = 0, note = i, velocity = 0))
+    default_color()
+
+#ボタンを押すと呼び出される関数
+def pgm(num):
+    print(f'[{time.ctime()}] pgm {num}')
+def pvw(num):
+    print(f'[{time.ctime()}] pvw {num}')
+def transtyle(num):
+    print(f'[{time.ctime()}] transition style {num}')
+def transiton(num):
+    print(f'[{time.ctime()}] transition {num}')
+def profile(num):
+    print(f'[{time.ctime()}] loading profile {num}')
+    set_list(num)
+    initialize()
+function = {'pgm': pgm, 'pvw': pvw, 'transtyle': transtyle, 'transition': transiton, 'profile':profile}
+
+set_list(1)
+initialize()
+while(true):
+    msg = inport.receive().hex().split()
+    if msg[2] == '00':
+        continue
+    if msg[1] in button_list:
+        function.get(function_list[str(msg[1])][0])(function_list[str(msg[1])][1])
