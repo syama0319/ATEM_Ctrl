0a96f6aa8c8ac3f8a0cac8141b236c95c871a24c
Sota YAMAGUCHI <fjhss0036@gmail.com>
2022-08-10 22:40:23 +0900

    update for Song-Genics-2022-summer-live

---
+++ set_pad.py
@@ -0,0 +1,12 @@
+import time
+import mido
+import csv
+import PyATEMMax
+import sys
+from typing import Dict, Any
+from mido import Message
+from tabulate import WIDE_CHARS_MODE, tabulate
+
+class Init_pad:
+    def __init__(self) -> None:
+        pass
